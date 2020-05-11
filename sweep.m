69537fb7438e83660391d678ea1dec083549dd41
JoãoPaina <jgpaina@gmail.com>
2020-05-11 15:32:14 -0300

    no sum(error())

--- sweep.m
+++ sweep.m
@@ -3,10 +3,9 @@ close all; clear all; clc;
 parameters_master;
 
 %total number of symble
-sig.n_sy = 1024;
+sig.n_sy = 4096;
 
-error(1:20) = 1;
-for ii = [21:sig.n_sy/2]
+for ii = [1:sig.n_sy/2]
     %number of symbles of the clock
     sig.n_sy_c = ii; 
     %number of symbles of the prbs
@@ -15,15 +14,15 @@ for ii = [21:sig.n_sy/2]
     
     main;
     
-    error(ii) = abs(sig.delay - block.delay);
+    error = abs(sig.delay - block.delay);
     
-    if sum(error(ii:-1:ii-20)) == 0
+    if error == 0
         break
     end
     clear block;
     global block
 end
-ii-20
-sig.n_sy-ii+20
+ii
+sig.n_sy-ii
 
 
